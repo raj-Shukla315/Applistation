5c8afb5e27c45697cb228141f714a714ade21b1f
raj-Shukla315 <rajshukla11204@gmail.com>
2025-09-01 23:59:48 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose")
 const DBconnection = () =>{
    return mongoose.connect(process.env.MONGODB_URL)
     .then(()=>console.log("MONGODB CONNECTION ESTABLISHED SUCCESSFULLY ✅"))
-    .catch((error)=>console.log("DB CONNECTION FAILED ❌"))
+     .catch((error) => {
+      console.error("DB CONNECTION FAILED ❌", error); // logs actual error
+      throw error; // optional: stop server if DB fails
+    });
 }
 
 module.exports = DBconnection;
