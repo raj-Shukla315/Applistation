9f2383fca53231ea98f5323ee9e5b61d9edae805
raj-Shukla315 <rajshukla11204@gmail.com>
2025-09-02 12:09:57 +0530

    Update dashboardCards.jsx

--- frontend/src/components/dashboardcomponents/dashboardCards.jsx
+++ frontend/src/components/dashboardcomponents/dashboardCards.jsx
@@ -15,7 +15,16 @@ const DashboardCards = ({totalApplications = 0, interviewsScheduled = 0 , offerR
       <Card icon= {FaEnvelope} title="Applications Submitted" value={totalApplications} />
       <Card icon={FaCalendarAlt} title="Interviews Scheduled" value={interviewsScheduled} />
       <Card icon={FaHandshake} title="Offers Received" value={offerRecieved} />
-      <Card icon={FaFileAlt} title="Resume ATS Score" value="0" />
+     <div className="bg-white rounded-xl shadow p-4 flex flex-col gap-2 dark:bg-gradient-to-b from-blue-700 to-blue-200">
+  <div className="text-blue-900 dark:text-white"><FaFileAlt size={28} /></div>
+  <p className="text-sm md:text-md text-gray-900 dark:text-white font-semibold">Check ATS</p>
+  <a
+    href="/dashboard/resume-score"
+    className="mt-2 px-3 py-1 bg-blue-600 text-white rounded-lg text-xs sm:text-sm font-semibold text-center hover:bg-blue-700 transition"
+  >
+    Go to ATS Score
+  </a>
+</div>
     </div>
   );
 };
