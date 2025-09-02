ca8e4b6fcb8fb694fadabe506270c2a844f56b82
raj-Shukla315 <rajshukla11204@gmail.com>
2025-09-02 12:18:54 +0530

    Update dashboardCards.jsx

--- frontend/src/components/dashboardcomponents/dashboardCards.jsx
+++ frontend/src/components/dashboardcomponents/dashboardCards.jsx
@@ -1,4 +1,5 @@
 import { FaEnvelope, FaCalendarAlt, FaFileAlt, FaHandshake } from "react-icons/fa"
+import { Link } from "react-router-dom";
 
 const Card = ({ icon:Icon, title, value }) => (
   <div className="bg-white rounded-xl shadow p-4 flex flex-col gap-1 dark:bg-gradient-to-b from-blue-700 to-blue-200
@@ -17,13 +18,13 @@ const DashboardCards = ({totalApplications = 0, interviewsScheduled = 0 , offerR
       <Card icon={FaHandshake} title="Offers Received" value={offerRecieved} />
      <div className="bg-white rounded-xl shadow p-4 flex flex-col gap-2 dark:bg-gradient-to-b from-blue-700 to-blue-200">
   <div className="text-blue-900 dark:text-white"><FaFileAlt size={28} /></div>
-  <p className="text-sm md:text-md text-gray-900 dark:text-white font-semibold">Check ATS</p>
-  <a
-    href="/dashboard/resume-score"
-    className="mt-2 px-3 py-1 bg-blue-600 text-white rounded-lg text-xs sm:text-sm font-semibold text-center hover:bg-blue-700 transition"
-  >
-    Go to ATS Score
-  </a>
+  <p className="text-sm md:text-md text-gray-900 font-semibold">Check ATS</p>
+ <Link
+  to="/dashboard/resume-score"
+  className="mt-2 px-3 py-1 bg-blue-600 text-white rounded-lg text-xs sm:text-sm font-semibold text-center hover:bg-blue-700 transition"
+>
+  Go to ATS Score
+</Link>
 </div>
     </div>
   );
